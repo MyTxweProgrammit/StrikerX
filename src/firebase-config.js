acac62fb8eeee472bdd5c2138657e5a0d1953d85
Txwe_18 <152937017+MyTxweProgrammit@users.noreply.github.com>
2026-04-05 07:14:42 +0000

    Edit Secret File

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,18 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqO7Tau8kF7Fgibz5lo8LRIgxI5KR-hcg",
-  authDomain: "strikerx-b00c5.firebaseapp.com",
-  projectId: "strikerx-b00c5",
-  storageBucket: "strikerx-b00c5.firebasestorage.app",
-  messagingSenderId: "748825446674",
-  appId: "1:748825446674:web:9b8cb0a4d70e23a6578bcb",
-  measurementId: "G-J04LPGN64P",
-  databaseURL: "https://strikerx-b00c5-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL
 };
 
 // Initialize Firebase
